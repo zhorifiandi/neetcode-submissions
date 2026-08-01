dee48f7a48489917b0f778525d90fad63d8e0cee
Ari P Zhorifiandi <ari@xendit.co>
2026-08-01 08:59:38 +0700

    Update: combination-target-sum - submission-1

--- Data Structures & Algorithms/combination-target-sum/submission-1.py
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -1,29 +1,20 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        # getCombs([],[2,5,6,9])
-        # -> 2: getCombs([2],[2,5,6,9])
-        #      -> 2: getCombs([2,2],[2,5,6,9])
-        #           -> 2: getCombs([2,2,2],[2,5,6,9])
-        #               -> 2: getCombs([2,2,2,2],[2,5,6,9]) 
-        #                   => stop (no possible ans)
-        #           -> 5 == 9 => return [2,2,5]
-        #           -> 6 x
-        #           -> 9 x
-        #      -> 5: getCombs([2,5],[5,6,9])
-        #           -> stop ( no possible ans)
-
         nums.sort()
-        def getCombs(base, nums):
-            answers = []
+        
+        def getNextCombinations(base, start):
+            solutions = []
             curSum = sum(base)
-            for i in range(len(nums)):
+            for i in range(start, len(nums)):
                 if curSum + nums[i] == target:
-                    answers.append(base + [nums[i]])
+                    solutions.append(base + [nums[i]])
                     break
                 
                 if curSum + nums[i] < target:
-                    answers.extend(getCombs(base + [nums[i]], nums[i:]))
+                    solutions.extend(
+                        getNextCombinations(base + [nums[i]], i)
+                    )
             
-            return answers
+            return solutions
 
-        return getCombs([], nums)
+        return getNextCombinations([], 0)
